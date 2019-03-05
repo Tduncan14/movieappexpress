a5b3fb70d12a1e4a2878aecf079ea383102eca40
unknown <nbatre1@yahoo.com>
2019-03-05 12:30:00 -0600

    keep moving forward

--- movieapp/app.js
+++ movieapp/app.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// the module router
 app.use('/', indexRouter);
 
 
--- movieapp/routes/index.js
+++ movieapp/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+const apiKey ='1c0d30ffc1bab82d086e8cd66b332f2e';
+
+const apiBaseUrl ='http://api.themoviedb.org/3';
+const nowPlayingUrl= `${apiBaseUrl}/movie/now_playing?api_keys=${apiKey}`;
+const imageBaseUrl ='http://image.tmdb.org/t/p/w300';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
 
 module.exports = router;
